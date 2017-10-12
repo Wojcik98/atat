f11d543df3567fd0e991373df5100c97273393d8
Wojcik <m4@qed.ai>
2017-10-12 14:04:17 +0200

    Refactor servo controller node

--- scripts/servo_controller.py
+++ scripts/servo_controller.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python2
 import serial
 import rospy
-from atat.msg import ServoPositions
+from atat.msg import ServosAngles
 
 
 def get_cmd_end():
@@ -13,38 +13,43 @@ def get_cmd_time(time):
     return [ord(c) for c in cmd]
 
 
-def get_cmd_impulse(channel, impulse):
+def get_cmd_impulse(impulse, channel):
     cmd = '#' + str(channel) + 'P' + str(impulse)
     return [ord(c) for c in cmd]
 
 
-def get_cmd_angle(channel, angle):
+def get_cmd_angle(angle, channel):
     impulse = int(float(angle)*2200./180. + 500)
-    return get_cmd_impulse(channel, impulse)
+    return get_cmd_impulse(impulse, channel)
 
 
-def set_servos(port, channels, angles, time):
-    cmd = []
-    for i in range(len(channels)):
-        cmd += get_cmd_angle(channels[i], angles[i])
+def set_servos(angles, time):
+    cmd = [get_cmd_angle(angle, channel) for channel, angle in angles]
     cmd += get_cmd_time(time)
     cmd += get_cmd_end()
     return port.write(cmd)
 
 
-def set_legs(port, channels, angles, times):
-    for i in range(4):
-        set_servos(port, channels[i], angles[i], times[i])
+def given2real(given_angles):
+    real_angles = []
+    for servo in given_angles.servos:
+        ch = servo.channel
+        angle = servo.angle
+        angle_zero = rospy.get_param('servo_' + str(ch) + '_angle_zero')    # TODO add to parameter server
+        servo_direction = int(rospy.get_param('servo_' + str(ch) + '_direction'))   # 1 or -1
+
+        real_angle = angle_zero + angle
+        if servo_direction == -1:
+            real_angle = 180. - real_angle
+
+        real_angles.append((ch, real_angle))
+    return real_angles
 
 
-def callback(servo_positions):
-    angles = [servo_positions.front_left, servo_positions.front_right,
-              servo_positions.back_left, servo_positions.back_right]
-    rospy.loginfo('Setting servos: ' + str(angles))
-    # TODO set this in parameter server
-    channels = [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]]
-    times = [200] * 4
-    set_legs(port, channels, angles, times)
+def callback(given_angles):
+    time = 200     # TODO read from parameter server
+    real_angles = given2real(given_angles)
+    set_servos(real_angles, time)
 
 
 def listener():
@@ -53,11 +58,10 @@ def listener():
     port.baudrate = 9600
     rospy.init_node('servo_controller')
 
-    rospy.Subscriber('servo_positions', ServoPositions, callback)
+    rospy.Subscriber('servos_angles', ServosAngles, callback)
 
     rospy.spin()
 
 
 if __name__ == '__main__':
     listener()
-
